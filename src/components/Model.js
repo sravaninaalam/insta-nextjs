746aeec029b8565b0ef5f0e0bbfc115a65ea695b
sravaninaalam <129977519+sravaninaalam@users.noreply.github.com>
2024-07-26 15:58:37 +0200

    upload button disabled by default

--- src/components/Model.js
+++ src/components/Model.js
@@ -8,7 +8,7 @@ export default function Model({isModalOpen,setIsModalOpen}) {
       <div className="shadow-lg border rounded-md p-10 flex flex-col items-center">
         <FaCamera className="text-4xl p-1 m-1 text-gray-500"/>
         <input type="text" maxLength='150' placeholder="enter caption here" className="w-full m-4 outline-none"/>
-        <button className="bg-red-600 text-white p-1 rounded w-full m-2">Upload post</button>
+        <button disabled className="disabled:bg-gray-300 disabled:cursor-not-allowed bg-red-600 text-white p-1 rounded w-full m-2">Upload post</button>
         <RxCross2 className="top-2 absolute right-2" onClick={()=>setIsModalOpen(false)}/>
       </div>
     </div>
